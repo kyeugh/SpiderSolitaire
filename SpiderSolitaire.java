8af30d404c48f5daecce3d2689f42af5e6124a0a
kyeugh <qvalador@gmail.com>
2020-04-14 13:10:41 -0400

    add menu bar and deal correct number of  piles

--- SpiderSolitaire.java
+++ SpiderSolitaire.java
@@ -15,17 +15,33 @@ class SpiderSolitaire {
         gbc.anchor = GridBagConstraints.NORTH;
         gbc.weighty = 1;
         /* game code goes here */
-        Pile piles[] = new Pile[5];
-        piles[0] = new Pile(deck, 6);
-        piles[1] = new Pile(deck, 6);
-        piles[2] = new Pile(deck, 6);
-        piles[3] = new Pile(deck, 5);
-        piles[4] = new Pile(deck, 5);
-        for (Pile pile : piles)
-            frame.add(pile, gbc);
+        for (int i = 0; i < 10; i++)
+            frame.add(new Pile(deck, 5));
         frame.setLayout(new FlowLayout(FlowLayout.CENTER));
         /* end game code */
-        frame.setSize(1100, 800);
+        /* menu bar */
+        JMenuBar menuBar = new JMenuBar();
+
+        JMenu gameMenu = new JMenu("Game");
+        gameMenu.setMnemonic('g');
+        JMenuItem restartMenu = new JMenuItem("Restart");
+        restartMenu.setMnemonic('r');
+        JMenuItem pauseMenu = new JMenuItem("Pause");
+        pauseMenu.setMnemonic('p');
+        gameMenu.add(restartMenu);
+        gameMenu.add(pauseMenu);
+
+        JMenu helpMenu = new JMenu("Help");
+        helpMenu.setMnemonic('h');
+        JMenuItem howtoMenu = new JMenuItem("How to play");
+        helpMenu.setMnemonic('t');
+        helpMenu.add(howtoMenu);
+
+        menuBar.add(gameMenu);
+        menuBar.add(helpMenu);
+        frame.setJMenuBar(menuBar);
+
+        frame.setSize(1250, 900);
         frame.setVisible(true);
     }
     public static void main(String[] args) {
